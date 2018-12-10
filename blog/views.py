3f1d68aa4dc0fa3b1db7b74eef0c25bd2f777ac9
bobnorigun <bobthedesigner@gmail.com>
2018-12-10 10:51:05 +0900

    new to edit

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request, pk):
+def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
