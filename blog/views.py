798d6dc24863e5c1cc3d2a316cf667326a28bded
bobnorigun <bobthedesigner@gmail.com>
2018-12-11 11:02:30 +0900

    new

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-#    if request.method == "POST":
+    if request.method == "POST":
         form = PostForm(request.POST)
 #        if form.is_valid():
 #            post = form.save(commit=False)
@@ -22,9 +22,9 @@ def post_new(request):
 #            post.published_date = timezone.now()
 #            post.save()
         return redirect('post_detail', pk=post.pk)
-#    else:
-#        form = PostForm()
-#    return render(request, 'blog/post_edit.html', {'form': form})
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
