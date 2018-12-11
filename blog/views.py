5a6c8f8495fed6456a2bfce4e92a428a6accbe30
bobnorigun <bobthedesigner@gmail.com>
2018-12-11 10:59:53 +0900

    new

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -15,16 +15,16 @@ def post_detail(request, pk):
 
 def post_new(request):
 #    if request.method == "POST":
-#        form = PostForm(request.POST)
+        form = PostForm(request.POST)
 #        if form.is_valid():
 #            post = form.save(commit=False)
 #            post.author = request.user
 #            post.published_date = timezone.now()
 #            post.save()
-#            return redirect('post_detail', pk=post.pk)
+        return redirect('post_detail', pk=post.pk)
 #    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+#        form = PostForm()
+#    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
